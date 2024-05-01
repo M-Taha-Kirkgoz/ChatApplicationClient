38b43239d2e82c63c53bf6ff05e87e5486efcb5b
Muhammet Taha KIRKGÖZ <m.taha.kirkgoz@gmail.com>
2024-05-01 15:50:59 +0300

    Client tarafı bitti.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,11 +5,12 @@ import { ChatModel } from '../../models/chat.model';
 import { HttpClient } from '@angular/common/http';
 
 import * as signalR from '@microsoft/signalr';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -22,6 +23,8 @@ export class HomeComponent {
   user = new UserModel();
 
   hub: signalR.HubConnection | undefined;
+
+  message: string = "";
 
   constructor(
     private http: HttpClient
@@ -41,6 +44,12 @@ export class HomeComponent {
         this.users.find(p => p.id == res.id)!.status = res.status;
         
       }); // Server tarafındaki "ChatHub" sınıfının içerisindeki "Connect" methodundan dönen cevabı "Users" ile dinlemeye başlar. (22. satır == Clients.All.SendAsync("Users", user);)
+
+      this.hub?.on("Messages", (res:ChatModel) => {
+        if ( this.selectedUserId == res.userId){
+          this.chats.push(res);
+        }
+      });
     });
   }
 
@@ -55,7 +64,25 @@ export class HomeComponent {
     this.selectedUserId = user.id;
     this.selectedUser = user;
 
-    //this.chats = Chats.filter(p=> p.toUserId == user.id && p.userId == "0" || p.userId == user.id && p.toUserId == "0");
+    this.http
+    .get(`https://localhost:7211/api/Chats/GetChats?userId=${this.user.id}&toUserId=${this.selectedUserId}`)
+    .subscribe((res:any) => {
+      this.chats = res;
+    });
+  }
+
+  sendMessage(){
+
+    const data = {
+      "userId": this.user.id,
+      "toUserId": this.selectedUserId,
+      "message": this.message
+    }
+
+    this.http.post<ChatModel>("https://localhost:7211/api/Chats/SendMessage", data).subscribe( res => {
+      this.chats.push(res);
+      this.message = "";
+    });
   }
 
   logout(){
